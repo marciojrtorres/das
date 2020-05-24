00826019b999da03c02e8ec595c136693a91f251
marciojrtorres <marciojrtorres@gmail.com>
2020-05-24 16:19:04 -0300

    initial

---
+++ quiz/covid/anwers.js
@@ -0,0 +1,19 @@
+const right = [
+  'Correto! 🙂',
+  'É isso aí! 😉', 
+  'Muito bem! 😀',
+  'Uau, perfeito! 😃',
+  'Sabendo tudo! 🤩',
+  'Orgulho da DAS! 🥰'
+];
+
+const wrong = [
+  'Oooops. 😯',
+  'Incorreto. 🙄',
+  'A próxima você acerta. 😬',
+  'É, não foi dessa vez. 😕',
+  'Não exatamente. 🧐',
+  'Opa, tá errado. 😟'
+];
+
+export {right, wrong};
---
+++ quiz/covid/questions.js
@@ -0,0 +1,127 @@
+const questions = [
+  {
+    question: 'O coronavírus pode ser transmitido através de um aperto de mão.',
+    options: [0, 1],
+    answer: 1,
+    explanation: 'O coronavírus se transmite por gotículas de saliva através do espirro, tosse ou catarro,que pode ocorrer, por exemplo, por aperto de mão ou toque e contato com objetos e superfícies contaminadas.'
+  },
+  {
+    question: 'Para evitar a transmissão do coronavírus, não é recomendado tomar chimarrão.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'Não precisa parar de tomar chimarrão! O que não pode é compartilhar com outras pessoas. Faça seu chimarrão individual, cada um com o seu!'
+  },
+  {
+    question: 'Cães e gatos são transmissores do coronavírus.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'Não há evidências científicas da transmissão do coronavírus de animais para seres humanos.'
+  },
+  {
+    question: 'Algumas medidas como tomar vitamina C, fazer gargarejo com água morna, sal e vinagre são eficazes contra a COVID-19.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'Até o momento, não há evidências científicas de qualquer medicamento, substância, vitamina, alimento ou vacina que previna ou combata a COVID-19.'
+  },
+  {
+    question: 'Não é necessário fazer a higiene dos produtos que trouxe do mercado, pois peguei eles com as mãos limpas.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'A higiene de qualquer produto que vem da rua é fundamental, pois não se sabe quem manipulou o produto antes de você.'
+  },
+  {
+    question: 'Com o uso da máscara estou 100% protegido, não necessitando outros cuidados.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'Mesmo utilizando a máscara, devo manter os cuidados de higiene, como lavar as mãos frequentemente com água e sabão, uso do álcool em gel e o isolamento social.'
+  },
+  {
+    question: 'Já que não tenho sintomas e me sinto bem, não há problemas em manter contato com meus amigos e parentes que moram longe de mim.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'Pode demorar até 14 dias para desenvolver os sintomas e, nesse período, pode-se transmitir a doença. Por isso é fundamental que todos façam o isolamento social, uma forma de cuidado a quem amamos!'
+  },
+  {
+    question: 'Devo procurar atendimento de saúde caso apresente febre alta e falta de ar.',
+    options: [0, 1],
+    answer: 1,
+    explanation: 'Procure atendimento se estiver com sintomas graves, como dificuldade para respirar e febre alta (a partir de 39°C). Além disso, se tiveres dúvidas, podes acessar a TeleCovid-19 (0800-642-4650) na cidade de Rio Grande.'
+  },
+  {
+    question: 'Se eu tiver apenas sintomas leves, semelhantes a um resfriado, posso sair de casa.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'Se você apresentar qualquer sintoma respiratório, deve permanecer em casa. O isolamento é a melhor forma de reduzir o contágio do coronavírus.'
+  },
+  {
+    question: 'O tempo de isolamento domiciliar para pessoas com suspeita ou confirmação de COVID-19 é de:',
+    options: ['10 dias', '14 dias'],
+    answer: 1,
+    explanation: 'A pessoa com confirmação ou suspeita de COVID-19 deve permanecer em isolamento domiciliar voluntário (em casa) durante 14 dias a partir da data do início dos sintomas.'
+  },
+  {
+    question: 'A pessoa com suspeita ou confirmação de COVID-19 não necessita ficar isolada de seus familiares, já que moram na mesma casa.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'A pessoa com suspeita ou confirmação de COVID-19 deve manter distância dos demais familiares, mesmo em casa, se possível em um cômodo ventilado e com a porta fechada.'
+  },
+  {
+    question: 'O risco de desenvolver complicações pela COVID-19 é maior em idosos e pessoas com doenças crônicas.',
+    options: [0, 1],
+    answer: 1,
+    explanation: 'Pessoas acima de 60 anos, bem como portadores de doenças crônicas como hipertensão, diabetes, problemas cardíacos ou pulmonares e com câncer estão mais propensos a desenvolver complicações como pneumonias e necessitar internação, inclusive em UTI.'
+  },
+  {
+    question: 'Se eu tiver problemas de saúde que não tem relação com o coronavírus, devo evitar procurar o serviço de saúde.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'Se você faz acompanhamento para alguma doença crônica, ou pré-natal, por exemplo, não deve abandonar as consultas! Mantenha o acompanhamento e converse com seu profissional de saúde sobre as medidas corretas a serem tomadas.'
+  },
+  {
+    question: 'A vacina da gripe não protege contra a COVID-19.',
+    options: [0, 1],
+    answer: 1,
+    explanation: 'É importante se vacinar contra a gripe para diminuir o volume de pessoas procurando atendimento médico e favorecer o diagnóstico preciso da COVID-19.'
+  },
+  {
+    question: 'O uso de luvas é uma das medidas de prevenção ao coronavírus.',
+    options: [0, 1],
+    answer: 0,
+    explanation: 'As luvas podem aumentar a chance de contaminação, pois as pessoas podem acabar tocando o rosto com elas ou se contaminando na hora de retirá-las. Por isso, a melhor prática de prevenção é a lavagem das mãos!'
+  },
+  {
+    question: 'O tempo de uso da máscara de pano é de, no máximo:',
+    options: ['2 horas', '4 horas'],
+    answer: 0,
+    explanation: 'Utilize a máscara de pano por no máximo 2 horas, e troque sempre que estiver úmida ou suja. Não esqueça de carregar uma máscara reserva e um saco plástico para armazenar.'
+  },
+  {
+    question: 'A máscara deve ser usada sempre que você precisar sair de casa, durante o tempo todo.',
+    options: [0, 1],
+    answer: 1,
+    explanation: 'Utilize a máscara durante todo o tempo, e evite tocar ou ajustar a máscara enquanto estiver usando. Ela deve cobrir totalmente o nariz e a boca.'
+  },
+  {
+    question: 'A forma correta de lavagem da máscara é com:',
+    options: [
+      'Sabão e água sanitária, deixar de molho por 30 minutos',
+      'Sabão e água quente, deixar de molho por 10 minutos'
+    ],
+    answer: 0,
+    explanation: 'A máscara deve ficar de molho por 30 minutos, após esfregar, enxaguar e secar bem, preferencialmente com ferro.'
+  },
+  {
+    question: 'Para uma lavagem de mãos ser eficaz, o tempo de duração deve ser, pelo menos:',
+    options: ['40 segundos', '15 segundos'],
+    answer: 0,
+    explanation: 'Uma boa lavagem de mãos deve durar pelo menos 40 segundos. Lembre-se de esfregar bem o dorso, entre os dedos, o polegar, debaixo das unhas e o punho.'
+  },
+  {
+    question: 'O tempo de duração do vírus em superfícies é de até:',
+    options: ['1 hora', '72 horas'],
+    answer: 1,
+    explanation: 'Estudos demonstraram que o vírus da COVID-19 pode sobreviver por até 72 horas em plástico e aço inoxidável, menos de 4 horas em cobre e menos de 24 horas em papelão. Por isso a limpeza dos objetos é fundamental!'
+  },
+];
+
+export default questions;
